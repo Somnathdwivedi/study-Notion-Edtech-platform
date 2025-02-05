91fea75f552fa6f45f09ec05cf0635864ba73e25
Somnath dwivedi <161061847+Somnathdwivedi@users.noreply.github.com>
2025-02-05 10:26:23 +0530

    Update database.js

--- Server/config/database.js
+++ Server/config/database.js
@@ -4,10 +4,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect = () =>{ 
-  mongoose.connect(process.env.MONGOD_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-})
+  mongoose.connect(process.env.MONGOD_URL)
 .then(() => console.log("DB Connection Successfully."))
 .catch((error) => {
   console.log("DB Connection failed.");
